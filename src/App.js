01245beb54591b3581c650f27ba2658f303c1be6
Dency631 <dency.patel@accolitedigital.com>
2023-06-06 18:12:28 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { Routes, Route, useNavigate, createSearchParams } from "react-router-dom"
+import {
+  Routes,
+  Route,
+  useNavigate,
+  createSearchParams,
+} from "react-router-dom";
 
-import { NavBar } from "./components/navbar"
-import { Products } from "./pages/products"
-import { Product } from "./pages/product"
-import { Cart } from "./pages/cart"
-import { NotFound } from "./pages/not-found"
-
-import { useCart } from './context/cart'
-import { Payment } from "./pages/payment"
-import { Login } from "./pages/login"
-import { Signup } from "./pages/signup"
+import { NavBar } from "./components/navbar";
+import { Products } from "./pages/products";
+import { Product } from "./pages/product";
+import { Cart } from "./pages/cart";
+import { NotFound } from "./pages/not-found";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useCart } from "./context/cart";
+import { Payment } from "./pages/payment";
+import { Login } from "./pages/login";
+import { Signup } from "./pages/signup";
 
 function App() {
-
   const navigate = useNavigate();
-  const { cartItemCount } = useCart()
+  const { cartItemCount } = useCart();
 
   const onSearch = (searchQuery) => {
-    navigate(`/?${createSearchParams({ q: searchQuery })}`)
-  }
+    navigate(`/?${createSearchParams({ q: searchQuery })}`);
+  };
 
   return (
     <>
@@ -27,9 +31,9 @@ function App() {
         <Route path="/" element={<Products />} />
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/payment" element={<Payment/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
